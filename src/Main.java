1c1713fc8b88b29b2c71dd57e80bed61cc9eb786
dredd <1misha.baltovsky@gmail.com>
2024-10-08 09:42:41 +0200

    change args for added.add() method

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         Adder adder = new Adder();
-        System.out.println(adder.add(20, 10));
+        System.out.println(adder.add(30, 10));
         Subtractor subtractor = new Subtractor();
         System.out.println(subtractor.subtract(10, 5));
     }
