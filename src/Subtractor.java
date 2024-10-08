cf99a13dfda582986f716eaf08b6fac5af6a19b6
JacobPy <jakub.puacz@euvic.pl>
2024-10-08 08:59:33 +0200

    subtractor class implementation

---
+++ src/Subtractor.java
@@ -0,0 +1,11 @@
+public class Substractor {
+
+    public int substract(int a, int b) {
+        return a - b;
+    }
+
+    public double substract(double a, double b) {
+        return a - b;
+    }
+
+}
